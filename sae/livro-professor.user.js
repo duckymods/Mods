25d5938df174ee1f55b188e6fe2d3ad90cb7be3f
duckymods <hiro.caffwy@gmail.com>
2024-06-01 17:08:10 -0300

    Update livro-professor.user.js

--- sae/livro-professor.user.js
+++ sae/livro-professor.user.js
@@ -9,25 +9,48 @@
 // @description 28/05/2024, 16:01:28
 // ==/UserScript==
 
-fetch("https://github.com/duckymods/Mods/blob/main/sae/livro-professor.user.js?raw=true") // Replace with your script URL
-  .then((response) => response.text())
-  .then((text) => {
-    // Extract version number from the script
-    const latestVersion = text.match(/@version\s+([0-9.]+)/)[1];
-    const currentVersion = GM_info.script.version;
-
-    // Compare versions
-    if (latestVersion > currentVersion) {
-      // Update available, prompt user or automatically update
-      if (confirm("An update is available. Do you want to install it?")) {
-        // Perform update
-        GM.setValue("script", text);
-        alert("Script updated successfully!");
-      }
-    } else {
-      console.log("No update available");
-    }
-  })
-  .catch((error) => {
-    console.error("Error checking for updates:", error);
+const sweetAlertScriptUrl =
+  "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.6/dist/sweetalert2.all.min.js";
+const sweetAlertCssUrl =
+  "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.6/dist/sweetalert2.min.css";
+const notiflixCssUrl =
+  "https://cdn.jsdelivr.net/npm/notiflix@3.2.7/src/notiflix.min.css";
+const notiflixJsUrl =
+  "https://cdn.jsdelivr.net/npm/notiflix@3.2.7/dist/notiflix-aio-3.2.7.min.js";
+function injectCSS(url) {
+  return new Promise((resolve, reject) => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = url;
+    link.onload = resolve;
+    link.onerror = reject;
+    document.head.appendChild(link);
   });
+}
+
+
+function injectScript(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+
+if (window.top !== window.self) {
+  let sstyle = document.createElement('style');
+  sstyle.textContent = 'a.professor.professor.professor { display: inline !important; }';
+  document.documentElement.appendChild(sstyle);
+  Promise.all([
+    injectScript(sweetAlertScriptUrl),
+    injectCSS(sweetAlertCssUrl),
+    injectCSS(notiflixCssUrl),
+    injectScript(notiflixJsUrl),
+  ])
+  .then(async () => {
+      Swal.fire("Livro Digital (Modo Professor)", "Atualização disponível!", "info")
+  })
+}
