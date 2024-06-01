dc5ad9f99bd049ce563347f922da5cdca957dd46
duckymods <hiro.caffwy@gmail.com>
2024-06-01 17:09:31 -0300

    Update livro-professor.user.js

--- sae/livro-professor.user.js
+++ sae/livro-professor.user.js
@@ -3,9 +3,8 @@
 // @namespace   Hiro Scripts
 // @match       https://livrodigital.sae.digital/@mount*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      Hiro
-// @require     https://github.com/caffwydev/cdn/blob/main/LivroDigital.js?raw=true
 // @description 28/05/2024, 16:01:28
 // ==/UserScript==
 
