c23b62d169b411ecb6bdee8c485ea0c68632192a
duckymods <hiro.caffwy@gmail.com>
2024-06-01 17:31:16 -0300

    Update livro-professor.js

--- sae/livro-professor.js
+++ sae/livro-professor.js
@@ -1,4 +1,4 @@
 // @version     1.0
-
-
-console.log("1.0!")
+let sstyle = document.createElement('style');
+sstyle.textContent = 'a.professor.professor.professor { display: inline !important; }';
+document.documentElement.appendChild(sstyle);
