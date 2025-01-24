bd8fcdaa89b799311d73135ae806ce8b636428b2
duckymods <hirocaffwy@gmail.com>
2025-01-24 19:40:46 -0300

    Create livro-enhanced.user.js

---
+++ sae/livro-enhanced.user.js
@@ -0,0 +1,122 @@
+// ==UserScript==
+// @name        Livro Digital Enhanced
+// @namespace   Hiro Scripts
+// @match       https://livrodigital.sae.digital/*
+// @grant       GM_xmlhttpRequest
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
+// @version     1.0 ALPHA
+// @author      Hiro
+// @description 23/01/2024, 14:00:00
+// ==/UserScript==
+
+if (true) {
+  const sweetAlertScriptUrl =
+    "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.6/dist/sweetalert2.all.min.js";
+  const sweetAlertCssUrl =
+    "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.6/dist/sweetalert2.min.css";
+  function injectCSS(url) {
+    if (window.top !== window.self) return;
+    return new Promise((resolve, reject) => {
+      if (window.top !== window.self) return resolve();
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      link.onload = resolve;
+      link.onerror = reject;
+      document.head.appendChild(link);
+    });
+  }
+
+  function injectScript(url) {
+    return new Promise((resolve, reject) => {
+      if (window.top !== window.self) return resolve();
+      const script = document.createElement("script");
+      script.src = url;
+      script.onload = resolve;
+      script.onerror = reject;
+      document.head.appendChild(script);
+    });
+  }
+
+  Promise.all([injectScript(sweetAlertScriptUrl), injectCSS(sweetAlertCssUrl)])
+    .then(async () => {
+      const Toast =
+        window.top === window.self
+          ? Swal.mixin({
+              toast: true,
+              position: "bottom-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              },
+            })
+          : { fire: (...a) => {} };
+      Toast.fire({
+        icon: "info",
+        title: "[Livro Digital Enhanced] Conectando ao servidor...",
+        description: "Sim",
+      });
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://raw.githubusercontent.com/duckymods/Mods/refs/heads/main/sae/livro-enhanced.js", // Replace with your script URL
+        onload: function (response) {
+          const text = response.responseText;
+          const storedScript = GM_getValue("script", "");
+          if (text !== storedScript) {
+            Toast.fire({
+              icon: "info",
+              title:
+                storedScript === ""
+                  ? "[Livro Digital Enhanced] Instalando..."
+                  : "[Livro Digital Enhanced] Atualizando...",
+            });
+            setTimeout(() => {
+              GM_setValue("script", text);
+              Toast.fire({
+                icon: "success",
+                title:
+                  storedScript === ""
+                    ? "[Livro Digital Enhanced] Instalado!"
+                    : "[Livro Digital Enhanced] Atualizado!",
+              });
+              eval(text);
+            }, 500);
+          } else {
+            Toast.fire({
+              icon: "success",
+              title: "Injeção sucedida!",
+            });
+            eval(storedScript);
+          }
+        },
+        onerror: function (error) {
+          Toast.fire({
+            icon: "error",
+            title: "Erro ao conectar ao servidor, injetando mesmo assim!",
+          });
+          const storedScript = GM_getValue("script", "");
+          if (storedScript === "")
+            return Toast.fire({
+              icon: "error",
+              title: "Script não encontrado localmente, falha total!",
+            });
+          eval(storedScript);
+          console.error("Error checking for updates:", error);
+        },
+      });
+    })
+    .catch((e) => {
+      console.error("Failed to load main!", e);
+      alert("Failed to connect! Injecting installed version!");
+      const storedScript = GM_getValue("script", "");
+      if (storedScript === "")
+        return alert("Local not found, failed to load Livro Digital Enhanced!");
+
+      eval(storedScript);
+    });
+}
